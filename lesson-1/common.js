ae72c68564eedcbe550160c55504f957088faef2
ITkach <ilatkacenko132@gmail.com>
2026-01-19 20:22:16 +0200

    19/01 lesson-1

---
+++ lesson-1/common.js
@@ -0,0 +1,55 @@
+// 1. Inline in html tags, <script>console.log('test')</script>, <script src="common.js"></script>
+
+// 2. Number 
+let num = 10;
+
+//3.
+let MyPhone = "Iphone 11";
+
+//4.
+let test = undefined;
+console.log(test);
+
+//5
+let nullTest =  null;
+console.log(typeof nullTest); // object
+
+//6. true/false
+
+//7. Camel Case, in English
+
+//8. 
+let firstName = 'Ilya';
+alert(firstName);
+
+//9. 
+let myFavoriteFilm = 'Limitless'
+console.log('My favorite film is ' + myFavoriteFilm);
+
+//10 
+let developer = true;
+
+//11 alert();, prompt(), confirm()
+
+//12
+prompt("What kind of dog do you have?");
+
+//13 
+let width = 5,
+    height = 10;
+
+let area = width * height;
+console.log(area);
+
+//14
+let first = "Hello",
+    second = "World";
+
+console.log(first + " " + second);
+
+//15 
+let a = Number(prompt("Введи перше число")),
+    b = Number(prompt("Введи друге число"));
+
+let sum = a + b;
+alert(sum);
